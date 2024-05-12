8e334f50806ea0a7e044afc5c4e91707524c2696
Alison <8661@holbertonstudents.com>
2024-05-12 22:53:27 +0000

    print comb3 and islower added, positive or negative and last digit corrected

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (10):
-    for h in range (10):
+for i in range(10):
+    for h in range(10):
         if (i != h and i < h) and i < 9:
             if (i == 8 and h == 9):
                 print('{0}{1}'.format(i, h))
