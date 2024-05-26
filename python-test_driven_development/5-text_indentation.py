268da7d648c53d2e99ef70d23edd2f9533097b83
Alison <8661@holbertonstudents.com>
2024-05-26 19:06:24 -0400

    text indentation and its test added

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+indents a text
+"""
 def text_indentation(text):
+    """
+    indents a text
+
+    Args:
+        text: text to be indented
+
+    Raises:
+        TypeError: if text isnt a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
