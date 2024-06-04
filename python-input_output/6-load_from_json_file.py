febf80f8c54ce72ca3e38952c27e288099b469ed
Alison <8661@holbertonstudents.com>
2024-06-03 21:51:48 -0400

    input output added

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,10 +6,12 @@ creates an object from a json file
 
 import json
 
+
 def load_from_json_file(filename):
     """
     creates an object from a json file
     """
+
 
     with open(filename) as file:
         return json.load(open(filename))
