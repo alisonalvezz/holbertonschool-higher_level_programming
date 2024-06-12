081a60bafb1134f2d0f641d682753edf9422f9e6
Alison Alvez <alisonalvez05@gmail.com>
2024-06-12 10:38:31 -0300

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence = None
+        sentence[0] = None
         lenght = 0
+        return(length, sentence[0])
     else:
         length = len(sentence)
         first = sentence[0]
