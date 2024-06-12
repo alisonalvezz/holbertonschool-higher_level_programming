0d8cb9d2e668e0309891c074f546fa306bd5460f
Alison Alvez <alisonalvez05@gmail.com>
2024-06-12 10:36:31 -0300

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,15 +6,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         a_first = tuple_a[0] if len(tuple_a) > 0 else 0
         a_second = 0
-    
+
     if len(tuple_b) >= 2:
         b_first = tuple_b[0]
         b_second = tuple_b[1]
     else:
         b_first = tuple_b[0] if len(tuple_b) > 0 else 0
         b_second = 0
-    
+
     first_add = a_first + b_first
     second_add = a_second + b_second
-    
+
     return (first_add, second_add)
