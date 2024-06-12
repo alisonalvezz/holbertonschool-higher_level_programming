6a860e8b70fb76c2f8d1382476eb858ad50a8492
Alison Alvez <alisonalvez05@gmail.com>
2024-06-12 10:27:45 -0300

    Update 4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,7 +5,7 @@ def only_diff_elements(set_1, set_2):
     for elements in set_1:
         if elements not in set_2:
             diff_set.add(elements)
-    
+
     for elements in set_2:
         if elements not in set_1:
             diff_set.add(elements)
