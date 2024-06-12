1710f2a44a11bf848d8cbd417d3c5c410f610c10
Alison Alvez <alisonalvez05@gmail.com>
2024-06-12 10:30:52 -0300

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
+
     best_key = None
     best_score = None
 
@@ -12,5 +12,5 @@ def best_score(a_dictionary):
         if best_score is None or value > best_score:
             best_score = value
             best_key = key
-        
+
     return best_key
