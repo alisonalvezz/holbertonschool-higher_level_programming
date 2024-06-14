6808b7300fda3be98fc4b26a76198785e5e8bb2c
Alison <8661@holbertonstudents.com>
2024-06-14 14:46:07 +0000

    data structures added, exceptions added, classes added

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,16 +4,43 @@ defines a rectangle
 """
 
 class Rectangle:
+    """
+    defines a rectangle
+    Attributes:
+        width: width of the rectangle
+        height: height of the rectangle
+    Raises:
+        TypeError: when the width or height arent int
+        ValueError: when widht or height are negative
+    """
     def __init__(self, width=0, height=0):
+        """
+        Initializes the rectangle with width and height
+
+        Attributes:
+            Width: the width of the rectangle
+            Height: the height of the rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        width of the rectangle
+        """
         return self.__width
         
     @width.setter
     def width(self, value):
+        """
+        width of the rectangle
+        Attributes:
+            Value: the value of the width
+        Raises:
+            TypeError: if width isnt an integer
+            ValueError: if width is negative
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -22,10 +49,21 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Height of the rectangle
+        Attributes:
+            Value: value of the height
+        Raises:
+            TypeError: if height isnt an integer
+            ValueError: if height is a negative
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,16 +6,33 @@ defines a rectangle
 
 
 class Rectangle:
+    """
+    defines a rectangle
+    """
     def __init__(self, width=0, height=0):
+        """
+        initializes the width and height of the rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        the width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        the width of the rectangle
+        Attributes:
+            Value: the value of the width
+        Raises:
+            TypeError: if width isnt an integer
+            ValueError: if width is a negative
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,10 +41,21 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Height of the rectangle
+        Attributes:
+            Value: value of the height
+        Raises:
+            TypeError: if height isnt an integer
+            ValueError: if height is a negative
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -35,12 +63,34 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """
+        Area of the rectangle
+        Returns:
+            width multiplied by height (the area)
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        Perimeter of the rectangle
+        Returns:
+            the sum of width and height multiplied by 2
+        """
         return 2 * (self.__width + self.__height)
-
+    
+    
     def __str__(self):
+        """
+        Returns a string representation of the object.
+
+        Returns a string that represents the rectangle using the '#' character
+        for each row and column that makes up the rectangle.
+
+        Returns an empty string if the width or height of the rectangle is 0.
+
+        Returns:
+            str: String representation of the rectangle.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
