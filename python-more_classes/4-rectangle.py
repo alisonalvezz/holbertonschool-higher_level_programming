269fcb0d667be08203985db94225e39506af8954
Alison <8661@holbertonstudents.com>
2024-06-14 14:54:49 +0000

    data structures added, exceptions added, classes added

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -6,16 +6,33 @@ defines a rectangle
 
 
 class Rectangle:
+    """
+    defines a rectangle
+    """
     def __init__(self, width=0, height=0):
+        """
+        initializes width and height
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        width of the rectangle
+        Attributes:
+            Value: value of the width
+        Raises:
+            TypeError: if value isnt an integer
+            ValueError: if value is a negative
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,10 +41,21 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Height of the rectangle
+        Attributes:
+            Value: value of the height of the rectangle
+        Raises:
+            TypeError: if value isnt an integer
+            ValueError: if value is a negative
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -35,15 +63,42 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """
+        the area of the rectangle
+        Returns:
+            the multiplication btw the width and the height
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        the perimeter of the rectangle
+        Returns:
+            the sum of width and height mul by 2
+        """
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """
+        Returns a string representation of the Rectangle object.
+
+        If either the width (__width) or the height (__height) of the rectangle
+        is 0, returns an empty string.
+
+        Returns:
+            str: A string representation of the rectangle using '#' characters
+                to represent each row and column.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
+        """
+        Returns a string representation of the Rectangle object for debugging.
+
+        Returns:
+        str: A string that includes the class name and the current values
+        of width (__width) and height (__height) of the rectangle.
+        """
         return f"Rectangle({self.__width}, {self.__height})"
