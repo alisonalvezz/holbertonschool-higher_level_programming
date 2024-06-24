a211364074ccac4acb7820bf4e93d3bdb0934f0f
Alison <8661@holbertonstudents.com>
2024-06-24 01:29:12 +0000

    sql added

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- updates score of bob
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- averages of all records in second table
+SELECT AVG(score) AS average FROM second_table;
