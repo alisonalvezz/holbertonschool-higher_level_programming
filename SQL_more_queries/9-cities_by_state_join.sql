4da6b3ee450bd9219ce158f3a1a7fe9f755d9cb8
Alison <8661@holbertonstudents.com>
2024-06-24 03:23:33 +0000

    more queries

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in a database
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
