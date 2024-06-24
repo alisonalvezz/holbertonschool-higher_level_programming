44a9284c2e3e1f4d7a3815f8b2526326d7356f05
Alison <8661@holbertonstudents.com>
2024-06-24 02:55:21 +0000

    more queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
