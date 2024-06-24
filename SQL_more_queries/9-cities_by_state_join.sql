616d79ef8d2d663396c85044c510bde0a4ee6a6f
Alison <8661@holbertonstudents.com>
2024-06-24 03:19:30 +0000

    more queries

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in a database
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
