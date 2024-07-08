e379cb1e275b034e5bc966b32b803bc620f603ab
Alison <8661@holbertonstudents.com>
2024-07-08 09:59:35 -0300

    i think this tests passed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,8 +24,8 @@ def matrix_divided(matrix, div):
     if not all(isinstance(i, list) for i in matrix) \
         or not all(isinstance(num, (int, float))
                    for i in matrix for num in i):
-        raise TypeError("matrix must be a matrix (list of lists) \
-                        of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(i) == len(matrix[0]) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
