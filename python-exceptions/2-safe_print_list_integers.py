9b06ddc97ed6bea9f8ceb16c84bd11546f8242d2
Alison <8661@holbertonstudents.com>
2024-07-08 08:22:04 -0300

    now exceptions is completed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    printed_count = 0
-    while count < x:
+
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[count]), end="")
-            printed_count += 1
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
         except (ValueError, TypeError):
-            pass
-        except IndexError:
-            break
-        count += 1
+            continue
     print()
-    return printed_count
+
+    return count
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = 0
     try:
         result = a / b
     except ZeroDivisionError:
         result = None
-        print("Inside result: None")
-    except Exception as e:
-        result = None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result {}".format(result))
         return result
