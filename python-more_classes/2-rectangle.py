349540c3a2d80e207081dbf902c6ec222e22bd48
Alison <8661@holbertonstudents.com>
2024-07-08 11:33:09 -0300

    rectangle added

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -78,4 +78,7 @@ class Rectangle:
         Returns:
             2 multiplied by the sum of width and height
         """
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
