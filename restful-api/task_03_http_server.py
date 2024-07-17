ec959f5962c900db0d38793e7ca0d949ad6d5e23
Alison <8661@holbertonstudents.com>
2024-07-17 15:19:10 -0300

    http server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Basic http"""
+
+import http.server
+import json
+
+class CustomRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(
+                {"name": "John", "age": "30", "city": "New York"}).encode()
+            )
+        
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(
+                {"version": "1.0", 
+                "description": "A simple API built with http.server"}).encode()
+            )
+    
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+def run(server_class=http.server.HTTPServer, 
+        handler_class=CustomRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
