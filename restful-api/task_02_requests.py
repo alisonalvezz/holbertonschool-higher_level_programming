913e8fd85020d9eca89a8ee25c5e47100076a5b1
Alison <8661@holbertonstudents.com>
2024-07-17 11:33:38 -0300

    task 02 requests

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,24 +6,32 @@ import csv
 import requests
 
 def fetch_and_print_posts():
+    """
+    fetches all posts from JSONPlaceholder and prints their titles
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print("Status code: {}".format(response.status_code))
+    print("Status Code: {}".format(response.status_code))
+    
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print("Title: {}".format(post['title']))
+            print(post['title'])
 
 def fetch_and_save_posts():
+    """
+    Fetches all posts from JSONPlaceholder and saves them into a CSV file
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
+    
     if response.status_code == 200:
         posts = response.json()
-        data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+        data = [{"id": post["id"], "title": post["title"], 
+                 "body": post["body"]} for post in posts]
 
-    with open("posts.csv", "w", newline='') as file:
-        keys = ["id", "title", "body"]
-        dict_writer = csv.DictWriter(file, fieldnames=keys)
+        with open("posts.csv", "w", newline='') as file:
+            keys = ["id", "title", "body"]
+            dict = csv.DictWriter(file, fieldnames=keys)
 
-        dict_writer.writeheader()
-        for post in data:
-            dict_writer.writerow(post)
+            dict.writeheader()
+            for post in data:
+                dict.writerow(post)
